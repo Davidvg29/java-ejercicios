30cba1d3599cb6b088e1d33486f5dfd73007508f
David Valdez Gramajo <davidpivito@gmail.com>
2024-06-29 18:47:06 -0300

    adding file calculadora con validaciones

---
+++ CalculadoraConValidaciones.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+
+        int operacion, numA, numB, resultado=0, pregunta=1;
+
+        Scanner teclado = new Scanner(System.in);
+
+       while (pregunta == 1) {
+
+        System.out.println("¡BIENVENIDOS A MI CALCULADORA!");
+        System.out.println("ingrese el numero de la operacion que desea resolver");
+        System.out.println("1 SUMAR");
+        System.out.println("2 RESTAR");
+        System.out.println("3 MULTIPLICAR");
+        System.out.println("4 DIVIDIR");
+        System.out.println("5 POTENCIA");
+
+        operacion = teclado.nextInt();
+
+        System.out.println("ingrese el primer numero");
+        numA = teclado.nextInt();
+        if(numA == 0){
+            System.out.println("No puedes hacer una operacion con 0");
+            System.out.println("ingrese el primer numero");
+            numA = teclado.nextInt();
+        }
+        System.out.println("ingrese el segundo numero");
+        numB = teclado.nextInt();
+        if (numB == 0) {
+            System.out.println("No puedes hacer una operacion con 0");
+            System.out.println("ingrese el segundo numero");
+            numB = teclado.nextInt();
+        }
+
+        if (operacion == 1) {
+            resultado = numA + numB;
+        }
+        if (operacion == 2) {
+            resultado = numA - numB;
+        }
+        if (operacion == 3) {
+            resultado = numA * numB;
+        }
+        if (operacion == 4){
+            resultado = numA / numB;
+        }
+        if (operacion == 5) {
+            int contador = 0;
+            resultado = 1;
+            while(contador < numB){
+                resultado = resultado * numA;
+                contador = contador + 1;
+            }
+        }
+
+        System.out.println("El resultado de tu operacion es: "+ resultado);
+
+        System.out.println("¿Desea realizar otra operacion? 0:NO 1:SI ");
+        pregunta = teclado.nextInt();
+        
+    }
+    teclado.close();
+
+    }
+}
+
+
+
+
+
+
+
+
+// import java.util.Scanner;
+// import Suma.suma;
+// import Resta.resta;
+// import Multiplicacion.multiplicacion;
+// import Division.division;
+
+// public class App {
+//     public static void main(String[] args){
+       
+//         new Suma.suma();
+//         new Resta.resta();    
+//         new Multiplicacion.multiplicacion();
+//         new Division.division();
+
+//     }
+// }
